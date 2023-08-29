42d61ece82e3e1d842eccb75f37a8ec66b025e68
J4nis05 <janis.roelli@gmail.com>
2023-08-29 22:43:34 +0200

    changed Configuration

--- src/utils/bin/commands.ts
+++ src/utils/bin/commands.ts
@@ -43,15 +43,6 @@ export const resume = async (args: string[]): Promise<string> => {
   return 'Opening resume...';
 };
 
-// Donate
-export const donate = async (args: string[]): Promise<string> => {
-  return `thank you for your interest. 
-here are the ways you can support my work:
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.paypal}" target="_blank">paypal</a></u>
-- <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.donate_urls.patreon}" target="_blank">patreon</a></u>
-`;
-};
-
 // Contact
 export const email = async (args: string[]): Promise<string> => {
   window.open(`mailto:${config.email}`);
@@ -101,16 +92,16 @@ export const whoami = async (args: string[]): Promise<string> => {
 };
 
 export const ls = async (args: string[]): Promise<string> => {
-  return `a
-bunch
-of
-fake
-directories`;
+  return `Never
+Gonna
+Give
+You
+Up`;
 };
 
 export const cd = async (args: string[]): Promise<string> => {
-  return `unfortunately, i cannot afford more directories.
-if you want to help, you can type 'donate'.`;
+  return `unfortunately, you are not allowed to use this command.
+you can try the 'vi' command for example.`;
 };
 
 export const date = async (args: string[]): Promise<string> => {
@@ -141,17 +132,33 @@ export const sudo = async (args?: string[]): Promise<string> => {
 // Banner
 export const banner = (args?: string[]): string => {
   return `
-█████        ███                       ███████████                                   
-░░███        ░░░                       ░█░░░███░░░█                                   
- ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
- ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
- ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
- ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
- ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
-░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+  █████ █████ █████              ███             █████    ██████████
+  ░░███ ░░███ ░░███              ░░░            ███░░░███ ░███░░░░░░█
+   ░███  ░███  ░███ █ ████████   ████   █████  ███   ░░███░███     ░ 
+   ░███  ░███████████░░███░░███ ░░███  ███░░  ░███    ░███░█████████ 
+   ░███  ░░░░░░░███░█ ░███ ░███  ░███ ░░█████ ░███    ░███░░░░░░░░███
+███   ░███        ░███░  ░███ ░███  ░███  ░░░░███░░███   ███  ███   ░███
+░░████████         █████  ████ █████ █████ ██████  ░░░█████░  ░░████████ 
+░░░░░░░░         ░░░░░  ░░░░ ░░░░░ ░░░░░ ░░░░░░     ░░░░░░    ░░░░░░░░  
 
-Type 'help' to see the list of available commands.
-Type 'sumfetch' to display summary.
-Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
 `;
 };
+
+
+// // Banner
+// export const banner = (args?: string[]): string => {
+//   return `
+// █████        ███                       ███████████                                   
+// ░░███        ░░░                       ░█░░░███░░░█                                   
+//  ░███        ████  █████ █████  ██████ ░   ░███  ░   ██████  ████████  █████████████  
+//  ░███       ░░███ ░░███ ░░███  ███░░███    ░███     ███░░███░░███░░███░░███░░███░░███ 
+//  ░███        ░███  ░███  ░███ ░███████     ░███    ░███████  ░███ ░░░  ░███ ░███ ░███ 
+//  ░███      █ ░███  ░░███ ███  ░███░░░      ░███    ░███░░░   ░███      ░███ ░███ ░███ 
+//  ███████████ █████  ░░█████   ░░██████     █████   ░░██████  █████     █████░███ █████
+// ░░░░░░░░░░░ ░░░░░    ░░░░░     ░░░░░░     ░░░░░     ░░░░░░  ░░░░░     ░░░░░ ░░░ ░░░░░ 
+
+// Type 'help' to see the list of available commands.
+// Type 'sumfetch' to display summary.
+// Type 'repo' or click <u><a class="text-light-blue dark:text-dark-blue underline" href="${config.repo}" target="_blank">here</a></u> for the Github repository.
+// `;
+// };
